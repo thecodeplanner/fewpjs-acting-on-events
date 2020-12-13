727e747c6337e5d2851dbd4c1be1c43fe97d276b
thecodeplanner <thecodeplanner@gmail.com>
2020-12-13 14:45:35 -0500

    commit tree 51f633113d9c00a7805e97102c1e4a88d03f7897

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+let dodge = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left-1}px`
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10)
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+
+
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+        // console.log(e.key)
+        moveDodgerLeft()
+    }
+
+    if (e.key === "ArrowRight") {
+        // console.log(e.key)
+        moveDodgerRight()
+    }
+})
+
